8a5c620c4370c6bc1b0e33f07167921125852fad
nikita.pometun <decembrist.org@gmail.com>
2018-10-31 20:32:12 +0300

    plugin-api-test fix for travis

--- kotlin2js-reflection-plugin-test/src/test/kotlin/org/decembrist/browser/Browser.kt
+++ kotlin2js-reflection-plugin-test/src/test/kotlin/org/decembrist/browser/Browser.kt
@@ -10,7 +10,7 @@ class Browser(private val port: Int) {
      */
     fun browse(): String {
         val webClient = WebClient()
-        val page: HtmlPage = webClient.getPage("http://0.0.0.0:$port/")
+        val page: HtmlPage = webClient.getPage("http://127.0.0.1:$port/")
         return page.body.asText()
     }
 
